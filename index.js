e0e1cd69f456f31e05848945a655e7826a995980
thavindu.ushan <thavinduushan@gmail.com>
2022-01-08 22:41:54 +0530

    start game on button click

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ let messageEl = document.getElementById("message-El");
 let sumEl = document.getElementById("sum-El");
 let cardsEl = document.getElementById("card-El");
 
-let firstCard;
-let secondCard;
-firstCard = Math.floor(Math.random() * 10) + 1;
-secondCard = Math.floor(Math.random() * 10) + 1;
-let sum = firstCard + secondCard;
-cardsEl.textContent = "Cards : " + firstCard + " " + secondCard;
-sumEl.textContent = "Sum : " + sum;
+//start game function
+function startGame() {
+  let firstCard;
+  let secondCard;
+  firstCard = Math.floor(Math.random() * 10) + 1;
+  secondCard = Math.floor(Math.random() * 10) + 1;
+  let sum = firstCard + secondCard;
+  cardsEl.textContent = "Cards : " + firstCard + " " + secondCard;
+  sumEl.textContent = "Sum : " + sum;
+}
