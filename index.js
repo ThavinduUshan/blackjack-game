6eb99b4b7f97a21e2b841c2263c6598c196055e0
thavindu.ushan <thavinduushan@gmail.com>
2022-01-08 22:47:14 +0530

    check whehter user has started the game or not

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+//check whether user clicked the start button or not
+let isAlive = false;
+
 //getting the dom elements
 let messageEl = document.getElementById("message-El");
 let sumEl = document.getElementById("sum-El");
 let cardsEl = document.getElementById("card-El");
 
+//cards array & sum
+let cards = [];
+sum = 0;
+
 //start game function
 function startGame() {
+  isAlive = true;
   let firstCard;
   let secondCard;
   firstCard = Math.floor(Math.random() * 10) + 1;
   secondCard = Math.floor(Math.random() * 10) + 1;
-  let sum = firstCard + secondCard;
+  sum = firstCard + secondCard;
   cardsEl.textContent = "Cards : " + firstCard + " " + secondCard;
   sumEl.textContent = "Sum : " + sum;
+}
+
+function newCard() {
+  if (isAlive) {
+  }
 }
